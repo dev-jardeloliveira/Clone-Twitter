ef137c567d2ff2cf3d89edeaa51cc05c6b0ea97f
Jardel Oliveira <dev.jardeloliveira@gmail.com>
2022-08-07 12:56:23 -0300

    Initial
    First commit

---
+++ app/src/main/java/com/example/clonetwitter/SplashScreenActivity.kt
@@ -0,0 +1,14 @@
+package com.example.clonetwitter
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashScreenActivity:AppCompatActivity(R.layout.activity_splash_screen) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
